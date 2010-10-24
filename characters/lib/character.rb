f56397e8be1a0b1eb266dd471014ca394ec5069b
Sean Copenhaver <sean.copenhaver@gmail.com>
2010-10-24 15:10:03 -0400

    refactored and got the code initial attribute values

--- characters/lib/character.rb
+++ characters/lib/character.rb
@@ -1,13 +1,47 @@
 # character
 
+class InvalidAttributeValuesError < Exception
+end
+
 class Character
+    def self.attrs_labels 
+        [:intelligence, :strength, :agility, :charisma]
+    end
+    
+    def self.min_attr_value
+        1
+    end
+
     attr_reader :intelligence, :strength, :agility, :charisma
 
-    def initialize 
-        @intelligence = 1
-        @strength = 1
-        @agility = 1
-        @charisma = 1
+    private_class_method :new
+
+    def self.create attrs={}
+        validate_attributes attrs
+       new attrs 
+    end
+
+    def initialize params={} 
+        @intelligence = params[:intelligence] || Character.min_attr_value
+        @strength = params[:strength] || Character.min_attr_value 
+        @agility = params[:agility] || Character.min_attr_value 
+        @charisma = params[:charisma] || Character.min_attr_value 
+    end
+
+    private
+    def self.validate_attributes attrs
+        return if attrs.keys.length == 0
+
+        total = 0
+        attrs_labels.each do |label|
+            total += attrs[label] || Character.min_attr_value 
+        end
+        raise InvalidAttributeValuesError unless total <= 20
+
+        attrs_labels.each do |label|
+            raise InvalidAttributeValuesError if attrs.key? label and attrs[label] < Character.min_attr_value
+        end
     end
 
 end
+
--- characters/spec/character_spec.rb
+++ characters/spec/character_spec.rb
@@ -1,9 +1,22 @@
 # character_spec
 require('character')
 
-describe Character, "when creating a default instance" do
+def attrs_label 
+    Character.attrs_labels
+end
+
+def create_default_attr_hash 
+    attrs = {}
+    attrs_label.each do |key|
+        attrs[key] = 1
+    end
+
+    attrs
+end
+
+describe Character, "when creating a default character" do
     before :all do
-        @character = Character.new
+        @character = Character.create 
     end
     
     it "has an intelligence attribute that starts at 1" do
@@ -22,3 +35,33 @@ describe Character, "when creating a default instance" do
       @character.charisma.should == 1
     end
 end
+
+describe Character, "when initializing attributes" do
+    it "raises error when total init values are over 20" do
+        attrs = create_default_attr_hash
+
+        attrs_label.each do |key|
+            attrs[key] = 20
+            lambda { Character.create attrs }.should raise_exception InvalidAttributeValuesError 
+        end
+    end
+
+    it "rases error when an attribute is less than 1" do
+        attrs = create_default_attr_hash
+        attrs_label.each do |key|
+            attrs[key] = -1
+            lambda { Character.create attrs }.should raise_exception InvalidAttributeValuesError
+        end
+    end
+
+    it "uses default value for any attribute not given" do
+        attrs = create_default_attr_hash
+        attrs_label.each do |key|
+            attrs.delete key
+            character = Character.create attrs
+            character.method(key).call.should == 1
+        end
+    end
+end
+
+
